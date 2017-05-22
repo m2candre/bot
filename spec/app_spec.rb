3f9434c9a1cc508a53e856dc26f1d86861470a61
Marcos Andre <m2c.andre@gmail.com>
2017-05-22 20:13:21 -0300

    Rspec tests created

---
+++ spec/app_spec.rb
@@ -0,0 +1,10 @@
+require_relative './spec_helper.rb'
+require_relative '../app.rb'
+
+RSpec.describe App  do
+  it 'Get sinatra response' do
+    get '/sinatra'
+    expect(last_response).to be_ok
+    #expect(last_response.status).to eq 200
+  end
+end
